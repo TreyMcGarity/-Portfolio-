0c9debf574d90d65a58ec935a9aec47bb846d9f0
Trey McGarity <trey.mcgarity99@gmail.com>
2020-10-26 18:34:35 -0500

    finished carousel feature

--- src/components/Module.jsx
+++ src/components/Module.jsx
@@ -1,14 +1,46 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Left from '../utils/images/left-arrow.png';
+import Right from '../utils/images/right-arrow.png';
 import '../sass/Module.scss';
 
 const Module = props => {
+    const minimum = 1
+    const maximum = 3
+    let [slide, setSlide] = useState(1);
+
+    const handleSlides = command => {
+        if (command === "dec") {
+            if (slide === minimum) {
+                setSlide(maximum)
+            } else {
+                setSlide(--slide)
+            }
+        }
+        else if (command === "inc") {
+            if (slide === maximum) {
+                setSlide(minimum)
+            } else {
+                setSlide(++slide)
+            }        
+        }
+    }
+
+    const handlePanel = value => {
+        switch (value) {
+          case 1: return <img className="image" src={props.first} alt="first"/>
+          case 2: return <img className="image" src={props.second} alt="second"/>
+          case 3: return <img className="image" src={props.third} alt="third"/>
+          default: return <img className="image" src={props.first} alt="first"/>
+        }
+    }
+
     return (
         <div className="module">
             <button onClick={() => {props.setShow(false)}}>Close</button>
-            <div>
-                <img className="image" src={props.first} alt="first"/>
-                <img className="image" src={props.second} alt="second"/>
-                <img className="image" src={props.third} alt="third"/>
+            <div className="carousel">
+                <img className="arrow" src={Left} alt="left-arrow" onClick={() => handleSlides("dec")}/>
+                {handlePanel(slide)}   
+                <img className="arrow" src={Right} alt="right-arrow" onClick={() => handleSlides("inc")}/>
             </div>
         </div>
     )
