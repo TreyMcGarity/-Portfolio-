f63c6c421179a17a4f3a2209c02142306b863143
Trey McGarity <trey.mcgarity99@gmail.com>
2020-10-28 16:02:35 -0500

    update sytling to use rem and em not px

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,6 +3,7 @@ import SaveTheAnimals from './SaveTheAnimals';
 import CoachMe from './CoachMe';
 import SmurfVillage from './SmurfVillage';
 import Bubbles from './Bubbles';
+import Card from './Card';
 import '../sass/Projects.scss';
 
 const Projects = () => {
@@ -11,11 +12,20 @@ const Projects = () => {
       <div className="text">
         <h2>Some of my Work!</h2>
       </div>
+      <h3>Front-End:</h3>
       <div className="projects">
         <SaveTheAnimals />
         <CoachMe />
         <SmurfVillage />
         <Bubbles />
+      </div>
+      <h3>Back-End:</h3>
+      <div className="projects">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
       </div>
     </div>
   );
