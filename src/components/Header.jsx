005be61097a2e82e1fa87899cd9791cab597c6a1
Trey McGarity <trey.mcgarity99@gmail.com>
2021-01-17 12:11:45 -0600

    moved social links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import Github from '../utils/images/github-logo.png';
+import LinkedIn from '../utils/images/linkedin-logo.png';
+import Facebook from '../utils/images/facebook-logo.png';
 import { Link } from 'react-router-dom';
 import '../sass/Header.scss';
 
@@ -9,7 +12,29 @@ const Header = () => {
                 <Link className="tab hover" to="/">My Projects</Link>
                 <Link className="tab hover" to="/about">About Myself</Link>
             </div>
-            <h1>Trey McGarity</h1>
+            <div className="name-links">
+                <h1>Trey McGarity</h1>
+                <div className="social-links hover">
+                    <a href="https://github.com/TreyMcGarity">
+                        <img 
+                        className="link github fade" 
+                        src={Github} 
+                        alt="github" />
+                    </a>
+                    <a href="https://www.linkedin.com/in/trey-mcgarity-3a0079196/">
+                        <img 
+                        className="link linkedin fade" 
+                        src={LinkedIn} 
+                        alt="linkedin" />
+                    </a>
+                    <a href="https://www.facebook.com/profile.php?id=100004535505847">
+                        <img 
+                        className="link facebook fade" 
+                        src={Facebook} 
+                        alt="facebook" />
+                    </a>
+                </div>
+            </div>
         </div>
     )
 }
--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
 import Header from '../components/Header.jsx';
-import Media from './Media.jsx';
 import Projects from './Projects.jsx';
 import About from './About.jsx';
 import '../sass/Portfolio.scss';
@@ -10,7 +9,6 @@ const Portfolio = () => {
   return (
     <div className="portfolio">
       <Header />
-      <Media />
         <div className="content">
             <Route exact path="/" component={Projects}/>
             <Route exact path="/about" component={About}/>
